bc0d7b457c1115558fb8a55c0dd058d3c72ed238
theofficialashmit@gmail.com <theofficialashmit@gmail.com>
2026-01-24 11:23:09 +0530

    docs: Add Git setup comment

---
+++ src/main/java/com/collegefest/booking/CollegeFestBookingApplication.java
@@ -0,0 +1,14 @@
+package com.collegefest.booking;
+// Git setup complete - ready to develop
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeFestBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeFestBookingApplication.class, args);
+	}
+
+}
